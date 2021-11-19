547e002151285b333ada21edb4b24ebb1309a2f0
jade-marte <jademarte7@gmail.com>
2021-11-18 19:21:41 -0500

    starting anew

---
+++ version_1/script.js
@@ -0,0 +1,9 @@
+// function OnClick(emlId){
+
+//     var elem = document.getElementById(emlId)
+//     if(elem && document.createEvent){
+//         var evt = document.createEvent("MouseEvents");
+//         evt.initEvent("click",true,false);
+//         elem.dispatchEvent(evt);
+//     }
+// }
