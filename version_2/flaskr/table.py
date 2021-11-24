3d5c4c4abd6b1316ab4045297931092f6ea1bccb
jade-marte <jademarte7@gmail.com>
2021-11-24 12:00:42 -0500

    adding to table

--- version_2/flaskr/table.py
+++ version_2/flaskr/table.py
@@ -17,3 +17,28 @@ def index():
         "ORDER BY employee_name DESC"
     ).fetchall()
     return render_template('table/index.html',table=table)
+
+@bp.route('/create',methods=('GET','POST'))
+@login_required
+def create():
+    if request.method =='POST':
+        client_name = request.form['client_name']
+        employee_name = request.form['employee_name']
+        session = request.form['session_time']
+        error = None
+
+        if not client_name:
+            error = 'client name is required'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO entry (client_name,pay, employee_id)'
+                'VALUES (?,?,?)'
+                (client_name,employee_name,g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('table.index'))
+    return render_template('payroll/create.html')
