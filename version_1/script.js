e3a71b0bf88c1f6645d6c73bce123d970e0d514e
jade-marte <jademarte7@gmail.com>
2021-11-27 17:32:25 -0500

    connected database

--- version_1/script.js
+++
@@ -1,9 +0,0 @@
-// function OnClick(emlId){
-
-//     var elem = document.getElementById(emlId)
-//     if(elem && document.createEvent){
-//         var evt = document.createEvent("MouseEvents");
-//         evt.initEvent("click",true,false);
-//         elem.dispatchEvent(evt);
-//     }
-// }
