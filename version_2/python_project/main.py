76292b2fc8a2428f8161e4ff1604da6e1c9c77af
jade-marte <jademarte7@gmail.com>
2021-11-30 11:25:16 -0500

    sessions

--- version_2/python_project/main.py
+++ version_2/python_project/main.py
@@ -10,13 +10,13 @@ mydb = mysql.connector.connect(
   password="ajbs1900",
   database="paystub"
 )
-print(mydb)
+# print(mydb)
 
 @app.route('/', methods=("POST", "GET"))
 def home():
     mycursor = mydb.cursor()
-    mycursor.excute("Select username,password,type From Employee")
-    users = [dict((mycursor.discription[i][0],value)\
+    mycursor.execute("Select username,password,type From Employee")
+    users = [dict((mycursor.description[i][0],value)\
                 for i,value in enumerate(row)) for row in mycursor.fetchall()]
     if request.method == 'POST':
         session.pop('user_id',None)
@@ -27,19 +27,32 @@ def home():
         if user and user.password == password:
             session['user_id'] = user.employee_id
             if user.type == 'employee':
+                print('employee logged in')
                 return redirect(url_for('employee.html'))
             else:
+                print('manager logged in')
                 return redirect(url_for('manager.html'))
+    
     return render_template('home.html', users=users)
 
 @app.route('/Signin',methods=("POST","GET"))
 def Signin():
     mycursor = mydb.cursor()
     if request.method == 'GET':
-        mycursor.excute("insert into employee"
-                    "values ")
-        users = []
-    return render_template('signin.html',users = users)
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        type = request.form['type']
+        pay = request.form['pay_rate']
+        department = request.form['department']
+
+        mycursor.excute("""insert into employee"
+                    "values (%s,%s,%s,%s,%s,%s,%s,%s)""", 
+                    None,username,password,None,type,name,department,pay)
+        mycursor.commit()   
+        print('inserted')
+    # return render_template('signin.html',users = users)
+    return render_template('signin.html')
 
 @app.route('/get_all_employee')
 def get_employees():
@@ -51,6 +64,7 @@ def get_employees():
     return (r[0])
 
 if __name__ == '__main__':
+    # home()
     # get_employees()
     app.run(debug=True)
     
