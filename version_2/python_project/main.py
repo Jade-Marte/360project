effced9192bd0d27855017ab6dd3703a6399d116
jade-marte <jademarte7@gmail.com>
2021-11-30 15:01:47 -0500

    login and signup

--- version_2/python_project/main.py
+++ version_2/python_project/main.py
@@ -1,7 +1,7 @@
 from sqlite3.dbapi2 import Cursor
 from flask import Flask, request, render_template, session, redirect,url_for
 import mysql.connector
-
+import json
 app = Flask(__name__)
 app.secret_key = 'paystub'
 mydb = mysql.connector.connect(
@@ -10,35 +10,46 @@ mydb = mysql.connector.connect(
   password="ajbs1900",
   database="paystub"
 )
-# print(mydb)
+mycursor = mydb.cursor()
 
+@app.route('/home', methods=("POST", "GET"))
 @app.route('/', methods=("POST", "GET"))
 def home():
-    mycursor = mydb.cursor()
-    mycursor.execute("Select username,password,type From Employee")
+    mycursor.execute("Select username,password,type,employee_id, manager_id From Employee")
     users = [dict((mycursor.description[i][0],value)\
                 for i,value in enumerate(row)) for row in mycursor.fetchall()]
+    print(users)
     if request.method == 'POST':
         session.pop('user_id',None)
         username = request.form['username']
         password = request.form['password']
 
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.employee_id
-            if user.type == 'employee':
+        user = [x for x in users if x['username'] == username][0]
+        if user and user['password'] == password:
+            session['user_id'] = user['employee_id']
+            session['manager_id'] = user['manager_id']
+            if user['type'] == 'employee':
                 print('employee logged in')
-                return redirect(url_for('employee.html'))
+                print(user['employee_id'])
+                return redirect('/employee')
             else:
                 print('manager logged in')
-                return redirect(url_for('manager.html'))
+                return redirect('/manager')
     
     return render_template('home.html', users=users)
 
-@app.route('/Signin',methods=("POST","GET"))
+@app.route('/test')
+def test():
+    query = querydb("select * from employee")
+    query2 = querydb("select * from sessions")
+    data = {'employee': query,'sessions': query2}
+    return json.dumps(data)
+
+
+@app.route('/Signup',methods=("POST","GET"))
 def Signin():
-    mycursor = mydb.cursor()
-    if request.method == 'GET':
+    print('checking')
+    if request.method == 'POST':
         name = request.form['name']
         username = request.form['username']
         password = request.form['password']
@@ -52,16 +63,41 @@ def Signin():
         mycursor.commit()   
         print('inserted')
     # return render_template('signin.html',users = users)
-    return render_template('signin.html')
+    return render_template('signup.html')
 
+
+
+@app.route('/employee')
+def employee():
+    employee_id = session['user_id']
+    data = querydb(f"select * from sessions where employee_id = {employee_id}")
+    # print(data)
+    return render_template('employee.html',data=data)
+
+
+@app.route('/manager')
+def manager():
+    manager_id = session['manager_id']
+    employee_data = querydb(f"select * from employee where manager_id = {manager_id}")
+    employee_ids = []
+    for x in employee_data:
+        employee_ids.append(x['employee_id'])
+    session_data = querydb("select * from sessions where employee_id in (%s) order by employee_id"% (', '.join(str(id) for id in employee_ids)))
+    return render_template('manager.html')
 @app.route('/get_all_employee')
 def get_employees():
-    mycursor = mydb.cursor()
     mycursor.execute("Select * FROM Employee")
     r = [dict((mycursor.description[i][0], value) \
                for i, value in enumerate(row)) for row in mycursor.fetchall()]
     # mycursor.connection.close()
-    return (r[0])
+    return (r)
+
+
+def querydb(sql):
+    mycursor.execute(sql)
+    r = [dict((mycursor.description[i][0], value) \
+               for i, value in enumerate(row)) for row in mycursor.fetchall()]
+    return (r)
 
 if __name__ == '__main__':
     # home()
--- version_2/python_project/static/script.js
+++ version_2/python_project/static/script.js
@@ -2,12 +2,12 @@
 // xhttp.open('GET','./get_all_employee',true)
 // console.log(xhttp.responseText)
 
-fetch('/get_all_employee').then(function (response) {
+fetch('/test').then(function (response) {
 	// The API call was successful!
 	return response.json();
 }).then(function (data) {
 	// This is the JSON from our response
-	console.log(data.name);
+	console.log(data);
 }).catch(function (err) {
 	// There was an error
 	console.warn('Something went wrong.', err);
